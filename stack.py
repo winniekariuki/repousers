0345987a429c5f3cb4a50a55fe893809b07146a0
winniekariuki <winniekariuki07@gmail.com>
2018-10-05 13:25:05 +0300

    added a constructor and a save function

--- stack.py
+++ stack.py
@@ -1,3 +1,10 @@
 from flask_bcrypt import Bcrypt
 
 class User:
+   def __init__(self, data = {}):
+        self.user_id = data.get('user_id')
+        self.username = data.get('username')
+        self.email = data.get('email')
+        self.b_crypt = Bcrypt()
+        if data.get('password'):
+            self.password = self.b_crypt.generate_password_hash(data.get('password')).decode('utf-8')
