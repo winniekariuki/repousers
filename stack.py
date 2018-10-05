7abb8a293d5ef0e8632066a7e59ebfb60b24294b
winniekariuki <winniekariuki07@gmail.com>
2018-10-05 13:35:06 +0300

    created a class named question and instantiated it

--- stack.py
+++ stack.py
@@ -7,4 +7,19 @@ class User:
         self.email = data.get('email')
         self.b_crypt = Bcrypt()
         if data.get('password'):
-            self.password = self.b_crypt.generate_password_hash(data.get('password')).decode('utf-8')
+            self.password = self.b_crypt.generate_password_hash(data.get('password')).decode('utf-8') def save(self):
+        pass
+        
+ class Question:
+
+    def __init__(self, data={}):
+        self.user_id = data.get('user_id')
+        self.question_id = data.get('question_id')
+        self.title = data.get('title')
+        self.body = data.get('body')
+
+    def save(self):
+        pass
+
+    def query(self):
+        pass
